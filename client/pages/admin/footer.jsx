e99e2ef9c523d77a30d98f775d3de1616beb6800
sean <sking21@students.towson.edu>
2016-11-09 18:25:19 -0500

    Changed Text on Homepage
    Changed Text on Homepage

--- client/pages/admin/footer.jsx
+++ client/pages/admin/footer.jsx
@@ -9,7 +9,7 @@ class Footer extends React.Component {
             <div className="footer">
                 <div className="container">
                     <span className="copyright pull-right">
-                        &#169; 2014 Acme, Inc.
+                        &#169; 2016 Group5, Inc.
                     </span>
                     <ul className="links">
                         <li><a href="/">Home</a></li>
