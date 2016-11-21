e50619b08b1b5ea518e501be836bd8e7b6614b88
jlaroc2 <jlaroc2@students.towson.edu>
2016-11-21 00:50:26 -0500

    Revert "Donations feedback"
    This reverts commit 1a2345b7f6e6bf0e626bac3955c19c207c0c148b.

--- server/web/contact/index.jsx
+++ server/web/contact/index.jsx
@@ -23,15 +23,7 @@ class ContactPage extends React.Component {
                         </p>
                         <i className="fa fa-reply-all bamf"></i>
                         <div>
-                            Group5 would like to thank our users for their generous donations.
-                            Here at Group5 we are striving to bring you the best content we
-                            can possibly provide. Thanks to our supporters we have received a
-                            total of $2,000!!! We have since spent $727 for development
-                            software for the team, as well as licences for use of some
-                            dependencies. We will continue to post on this page in order
-                            to provide you all with updates as to what your money is going
-                            toward. Thank you everyone who donated, you are the reason we are
-                            able to keep doing what we're doing.
+                            Subscriptions here.........
                         </div>
                     </div>
                 </div>
