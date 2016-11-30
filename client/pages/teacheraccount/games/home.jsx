f12484cd72f2fa490cefe62a614353e89b5bf4c1
jlaroc2 <jlaroc2@students.towson.edu>
2016-11-29 21:12:02 -0500

    Teacher page
    Still working on getting the teacher page working, this isnt a finished
    product in the slightest.

---
+++ client/pages/teacheraccount/games/home.jsx
@@ -0,0 +1,63 @@
+'use strict';
+const Moment = require('moment');
+const React = require('react');
+
+
+class HomePage extends React.Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = this.getThisMoment();
+    }
+
+    componentDidMount() {
+
+        this.interval = setInterval(this.refreshTime.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+
+        clearInterval(this.interval);
+    }
+
+    refreshTime() {
+
+        this.setState(this.getThisMoment());
+    }
+
+    getThisMoment() {
+
+        const thisMoment = Moment();
+
+        return {
+            second: thisMoment.format('ss'),
+            minute: thisMoment.format('mm'),
+            hour: thisMoment.format('HH'),
+            day: thisMoment.format('DD'),
+            month: thisMoment.format('MM'),
+            year: thisMoment.format('YYYY')
+        };
+    }
+
+    render() {
+
+        return (
+            <section className="section-home container">
+                <div className="row">
+                    <div className="col-sm-7">
+                        <h1 className="page-header">Insert the game here!!</h1>
+                        <div className="row">
+                            <div className="col-sm-6">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+
+module.exports = HomePage;
---
+++ client/pages/teacheraccount/home.jsx
@@ -0,0 +1,69 @@
+'use strict';
+const Moment = require('moment');
+const React = require('react');
+
+
+class HomePage extends React.Component {
+    constructor(props) {
+
+        super(props);
+
+        this.state = this.getThisMoment();
+    }
+
+    componentDidMount() {
+
+        this.interval = setInterval(this.refreshTime.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+
+        clearInterval(this.interval);
+    }
+
+    refreshTime() {
+
+        this.setState(this.getThisMoment());
+    }
+
+    getThisMoment() {
+
+        const thisMoment = Moment();
+
+        return {
+            second: thisMoment.format('ss'),
+            minute: thisMoment.format('mm'),
+            hour: thisMoment.format('HH'),
+            day: thisMoment.format('DD'),
+            month: thisMoment.format('MM'),
+            year: thisMoment.format('YYYY')
+        };
+    }
+
+    render() {
+
+        return (
+            <section className="section-home container">
+                <div className="row">
+                    <div className="col-sm-7">
+                        <h1 className="page-header">My account</h1>
+                        <div className="row">
+                            <div className="col-sm-6">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-5">
+                        <h1 className="page-header">Throttle guage</h1>
+                        <div className="text-center">
+                            <i className="fa fa-dashboard bamf"></i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+
+module.exports = HomePage;
