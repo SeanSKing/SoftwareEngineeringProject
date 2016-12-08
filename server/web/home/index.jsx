a5cb0a66fe75b15cdde8d8b84ed3ca628bd1b83a
iqbal-singh <isingh2@students.towson.edu>
2016-12-08 15:07:38 -0500

    added donation summary to front page

--- server/web/home/index.jsx
+++ server/web/home/index.jsx
@@ -10,14 +10,14 @@ class HomePage extends React.Component {
 
         return (
             <Layout
-                title="MathGame"
+                title="Math Attack"
                 neck={neck}
                 activeTab="home">
 
                 <div className="jumbotron">
-                    <h1>MathGame</h1>
+                    <h1>Math Attack</h1>
                     <div>
-                        <p className="lead">Welcome To MathGame!!</p>
+                        <p className="lead">Welcome To Math Attack!</p>
                         <div>
                             <a className="btn btn-primary btn-lg" href="/signup">
                                 Create an account
@@ -29,11 +29,49 @@ class HomePage extends React.Component {
                         </div>
                     </div>
                 </div>
+              
+                
+
+
+
                 <div className="row">
+         <div className="container">
+                    
+                    <div className="well well-lg">
+                    <h2>Donation Summary</h2>
+                     <div className="panel-heading">
+                     Recent changes to state law force the Board of Education to disclose the details of all donations associated with this project along with a brief explanation of how the donations were used.
+                      This report should be available via the public-facing portion of the website.  
+                     </div>
+                     <div className="panel panel-body">
+                    <table className="table">
+                    <tr>
+                    <th>Donation Amount recieved: </th>
+                    <th>Used for: </th>
+                    </tr>
+                    <tr>
+                    <td>$5</td>
+                    <td>This donation went to help with school supplies</td>
+                    </tr>
+                    <tr>
+                    <td>$50</td>
+                    <td>test</td>
+                    </tr>
+                    <tr>
+                    <td>$20</td>
+                    <td>test</td>
+                    </tr>
+
+                    </table>
+                     </div>
+
+                    </div>
+
+                    </div>
                     <div className="col-sm-4">
                         <div className="panel panel-default">
                             <div className="panel-body">
-                                <h3>About MathGame</h3>
+                                <h3>About Math Attack</h3>
                                 <p>
                                     Click "Learn more" here if you want to know more about us and our site.
                                 </p>
@@ -70,6 +108,10 @@ class HomePage extends React.Component {
                             </div>
                         </div>
                     </div>
+
+
+
+
                 </div>
             </Layout>
         );
