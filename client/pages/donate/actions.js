b76953e664eb4a2a72d0e383646d5e699bc3bba3
iqbal-singh <isingh2@students.towson.edu>
2016-12-11 09:44:19 -0500

    donate page

--- client/pages/donate/actions.js
+++ client/pages/donate/actions.js
@@ -13,20 +13,20 @@ paypal.configure({
 });
 
 //json with payment information
-var donation_data = {
+var create_payment_json = {
     "intent": "sale",
     "payer": {
         "payment_method": "paypal"
     },
     "redirect_urls": {
         "return_url": "http://mathattack.herokuapp.com/", //need to create a thank you page
-        "cancel_url": "http://mathattack.herokuapp.com/donate" // back to donation page if it fails
+        "cancel_url": "http://mathattack.herokuapp.com/contact" // back to donation page if it fails
     },
     "transactions": [{
         "item_list": {
             "items": [{
-                "name": "Donation to Group 5",
-                "sku": "grp5-mathattack",
+                "name": "$5 donation to help Math Attack",
+                "sku": "item",
                 "price": "5.00",//donation amount is $5  
                 "currency": "USD",
                 "quantity": 1
@@ -36,14 +36,21 @@ var donation_data = {
             "currency": "USD",
             "total": "5.00" //donation amount is $5  
         },
-        "description": "A donation of $5.00 to group 5."
+        "description": "Donation to Group5."
     }]
 };
 
-paypal.payment.create(donation_json, function (error, payment) {
+paypal.payment.create(create_payment_json, function (error, payment) {
     if (error) {
         throw error;
     } else {
+        //console.log("Create Payment Response");
+        //console.log(payment);
+        //console.log("\n\n\n\n\n");
+        //console.log("redirect url\n");
+        //console.log(payment.links[1].href);
+        //console.log("\n--------------------------------------");
+
         url = payment.links[1].href; //use paypal url from payment object
     }
 });
