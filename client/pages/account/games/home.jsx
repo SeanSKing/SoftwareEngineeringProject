1be288fb8ff697999720c0b6e809d9966377fce6
Grant Fernandez <lferna2@students.towson.edu>
2016-12-14 17:16:08 -0500

    trying to add game

--- client/pages/account/games/home.jsx
+++ client/pages/account/games/home.jsx
@@ -44,6 +44,13 @@ class HomePage extends React.Component {
 
         return (
             <section className="section-home container">
+            <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-3">Welcome to Math Attack!</h1>
+        <p class="para">Select the type of problem you would like to practice.</p>
+
+      </div>
+    </div>
                 <div className="row">
                     <div className="col-sm-7">
                         <h1 className="page-header">Insert the game here!!</h1>
